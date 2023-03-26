948ba3831d726de45a566976170da957b624b5f1
Matthew <matthew7419@gmail.com>
2023-03-26 16:58:06 -0400

    Initial Commit

---
+++ MemorizationGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemorizationGame
+//
+//  Created by Matthew Low on 2023-03-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemorizationGame/MemorizationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizationGameApp.swift
+//  MemorizationGame
+//
+//  Created by Matthew Low on 2023-03-26.
+//
+
+import SwiftUI
+
+@main
+struct MemorizationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
